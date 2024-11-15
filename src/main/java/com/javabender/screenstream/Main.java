a75cb519e4b9b3947ad145958d27376450abbfc4
dkrepkh <51156498+dkrepkh@users.noreply.github.com>
2024-11-15 20:05:38 +0200

    changes to the screen capture class and adding a new way to control threads

--- src/main/java/com/javabender/screenstream/Main.java
+++ src/main/java/com/javabender/screenstream/Main.java
@@ -4,13 +4,16 @@ import com.javabender.screenstream.stream.management.ScreenCapture;
 import com.javabender.screenstream.stream.management.StreamManager;
 import com.javabender.screenstream.stream.management.VideoStream;
 
-import java.awt.*;
+import org.bytedeco.javacv.Frame;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
-    private static final int FRAME_RATE = 10;
+    private static final int FRAME_RATE = 30;
     public static void main(String[] args) throws AWTException, IOException {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the ip address: ");
@@ -19,14 +22,8 @@ public class Main {
         if(ipAddress.isEmpty()) {
             ipAddress = "127.0.0.1";
         }
+        BlockingQueue<Frame> frames = new LinkedBlockingQueue<Frame>();
         StreamManager streamManager = new StreamManager(ipAddress, FRAME_RATE);
         streamManager.startStream();
-        /*try {
-            Thread.sleep(10000);
-            System.out.println("стрим останавливается");
-            streamManager.stopStream();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
     }
 }
